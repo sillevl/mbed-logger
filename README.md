25996be55d8ce4ba5f7f78a67b8e1b1c0df047b9
Sille Van Landschoot <info@sillevl.be>
2021-04-12 13:51:35 +0200

    working implementation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Serial Logger/Debugger for mbed
+
+```cpp
+#include "logger.h"
+
+// ...
+
+#define LOGGER_MODULE_NAME "DEMO"
+unsigned loggerLevel = LOGGER_LEVEL_DEBUG;
+
+// ...
+
+EMERGENCY("emergency");
+ALERT("alert");
+ERROR("error");
+WARNING("warning");
+NOTICE("notice");
+INFO("info");
+DEBUG("notice");
+```
+
+## Example output
+
+![Example output](./img/example_output.png)
--- logger.h
+++ logger.h
@@ -1,6 +1,3 @@
-// #define LOGGER_MODULE_NAME "<YourModuleName>"
-
-// -- within a header file named something like trace.h --
 enum {
     LOGGER_LEVEL_EMERGENCY,
     LOGGER_LEVEL_ALERT,
@@ -10,57 +7,33 @@ enum {
     LOGGER_LEVEL_INFO,
     LOGGER_LEVEL_DEBUG
 };
-/* each time we compile or run the program,
-* we determine what the trace level is.
-* this parameter is available to the macros
-* without being explicitly passed to them*/
 
 extern unsigned loggerLevel;
 
-// ...
-
-// Our first macro prints if the trace level we selected
-// is LOGGER_LEVEL_DEBUG or above.
-// The traceLevel is used in the condition
-// and the regular parameters are used in the action that follows the IF
-
-
 #define EMERGENCY(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_EMERGENCY) \
-        { printf("[%12.12s][EMERGENCY] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_EMERGENCY) \
+        { printf("\x1b[91m[%12.12s][EMERGENCY]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define ALERT(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_ALERT) \
-        { printf("[%12.12s][ALERT] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_ALERT) \
+        { printf("\x1b[35m[%12.12s][ALERT    ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define ERROR(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_ERROR) \
-        { printf("[%12.12s][ERROR] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_ERROR) \
+        { printf("\x1b[31m[%12.12s][ERROR    ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define WARNING(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_WARNING) \
-        { printf("[%12.12s][WARNING] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_WARNING) \
+        { printf("\x1b[32m[%12.12s][WARNING  ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define NOTICE(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_NOTICE) \
-        { printf("[%12.12s][NOTICE] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_NOTICE) \
+        { printf("\x1b[33m[%12.12s][NOTICE   ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define INFO(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_INFO) \
-        { printf("[%12.12s][INFO] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
+    { if (loggerLevel >= LOGGER_LEVEL_INFO) \
+        { printf("\x1b[36m[%12.12s][INFO     ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
 
 #define DEBUG(formatstring, ...) \
-    { if (traceLevel <= LOGGER_LEVEL_DEBUG) \
-        { printf("[%12.12s][DEBUG] " formatstring "\r\n", LOGGER_MODULE_NAME, __VA_ARGS__); } }
-
-
-
-
-
-
-// #define LOG(x, ...) \
-//     { printf("\x1b[34m%12.12s: \x1b[39m"x"\x1b[39;49m\r\n", \
-//     LOGGER_MODULE_NAME, ##__VA_ARGS__); fflush(stdout); }
-// #define WARN(x, ...) \
-//     { printf("\x1b[34m%12.12s: \x1b[33m"x"\x1b[39;49m\r\n", \
-//     LOGGER_MODULE_NAME, ##__VA_ARGS__); fflush(stdout); }
+    { if (loggerLevel >= LOGGER_LEVEL_DEBUG) \
+        { printf("\x1b[32m[%12.12s][DEBUG    ]\x1b[39m " formatstring "\r\n", LOGGER_MODULE_NAME, ##__VA_ARGS__); } }
